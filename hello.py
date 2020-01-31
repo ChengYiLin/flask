db9f14ea39afe4ece08e076854c327633be3ac2a
cyl <cheng.yi.lin.217@gmail.com>
2020-01-31 13:04:16 +0800

    Day4 - flask bootstrap wtf

--- hello.py
+++ hello.py
@@ -5,9 +5,19 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
+
 app.config['SECRET_KEY'] = 'this is a key'
 
 class first_form(FlaskForm):
     name = StringField(label='What is your name', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
+@app.route('/', methods=["GET", "POST"])
+def index():
+    name = None
+    form = first_form()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template("index.html", form=form, name=name)
